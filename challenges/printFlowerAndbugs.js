1defc0af386412a3c5876dad100c77daf84a6f09
Ahmat Izzedine <96932196+Izzadine@users.noreply.github.com>
2025-08-09 14:34:04 +0000

    print flower and bugs from api challenge solved

---
+++ challenges/printFlowerAndbugs.js
@@ -0,0 +1,35 @@
+/*
+   Oh no, our emoji flower bed is infested with mammals, trees and leaves!
+   Without changing the API url, write a function to transform your 
+   data before it's displayed. The function should eliminate
+   everything but bugs and flowers. Use your function in the API call.  
+   
+   Hint: Be sure to console the data to see what properties can help you do this!
+*/ 
+
+const api = 'https://apis.scrimba.com/emojihub/api/all/category/animals-and-nature';
+const flowerBed = document.querySelector('.emoji-flower-bed');
+
+function clearTheGarden(arr){
+   return arr.filter(flower=>{
+       return (flower.group==="plant flower") || flower.group==="animal bug"
+        
+    })
+    
+}
+
+
+fetch(api) 
+    .then(response => response.json())
+    .then((data) => {
+        const flowerAndbug = clearTheGarden(data)
+        flowerAndbug.forEach(emoji => {
+            flowerBed.innerHTML += `<li>${emoji.htmlCode}</li>`;
+                
+            
+        });    
+    })
+    .catch(err => console.log(err));
+
+
+
